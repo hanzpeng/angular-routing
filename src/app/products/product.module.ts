e95d7d1af549c4034cf8110f3b22084313f8cc97
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-16 11:17:23 -0700

    add child route

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,9 +1,13 @@
+import { ProductData } from './product-data';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
+
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -13,13 +17,33 @@ import { SharedModule } from '../shared/shared.module';
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'products/:id/edit', component: ProductEditComponent }
+      {
+        path: 'products/:id/edit',
+        component: ProductEditComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'info',
+            pathMatch: 'full'
+          },
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
+          }
+        ]
+      }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
