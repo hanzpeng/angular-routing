e1a1bc9636daf1a204828319bdf38eeb97fb51b2
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-16 23:45:44 -0700

    add router animation and tracing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 import { Component } from '@angular/core';
-import { slideInAnimation} from './app.animation';
+import { slideInAnimation } from './app.animation';
 
 import { AuthService } from './user/auth.service';
 
@@ -12,6 +12,7 @@ import { AuthService } from './user/auth.service';
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
+  loading = true;
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -24,8 +25,25 @@ export class AppComponent {
     return '';
   }
 
-  constructor(private authService: AuthService,
-    private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRuterEvent(routerEvent);
+    });
+
+  }
+
+  checkRuterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError) {
+        this.loading = false;
+    }
+  }
 
   logOut(): void {
     this.authService.logout();
