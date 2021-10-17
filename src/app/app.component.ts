31c67454721ffc112728c72dffc43c4d83613011
Hanz Zhang <hanzzhan@microsoft.com>
2021-10-16 23:17:36 -0700

    add route animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { slideInAnimation} from './app.animation';
 
 import { AuthService } from './user/auth.service';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [slideInAnimation]
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
